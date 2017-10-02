c78ffa90f5654b965d129c3c1a11bb619bd1f7a1
smbrink <smbrink@swbell.net>
2017-10-02 16:03:36 -0500

    Update and rename weight_bin_code.py to weight_height_bin_code.py

--- weight_height_bin_code.py
+++ weight_height_bin_code.py
@@ -1,4 +1,4 @@
-weight_bin code
+# weight_bin code
 
 import pandas as pd
 
@@ -9,6 +9,8 @@ weight_labels = [160, 170, 180, 190, 200, 210, 220, 230]
 baseball['weight_bin'] = pd.cut(baseball['weight'], weight_bins, labels=weight_labels)
 
 baseball.to_csv('baseball_data_deleted0_new.csv', index=False)
+
+# added height bin code
 
 baseball = pd.read_csv('baseball_data_deleted0_new.csv')
 
